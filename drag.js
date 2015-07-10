5c0f8b5ac6b6ca69b1057b47ee890aef5df621b8
lk3407105 <997861461@qq.com>
2015-07-10 08:17:20 +0800

    添加

--- drag.js
+++ drag.js
@@ -7,5 +7,8 @@ function drag(id) {
 	obj.onmousedown = function(ev) {
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
+		document.oumousemover = function() {
+			
+		};
 	};
 }
